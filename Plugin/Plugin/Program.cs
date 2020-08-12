c8c3bb60a2547f6ee734c25d01d703fff8493776
Alberto Guerrero <alberto.guerrero@modmed.com>
2020-08-12 17:29:20 -0400

    Initial Commit

---
+++ Plugin/Plugin/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plugin
+{
+	// Modify the code to allow a single collection containing both circles & squares.
+	// There must be only one loop to generate the output from the Draw method.
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Circle> circles = new List<Circle>();
+			circles.Add(new Circle { X = 1, Y = 2, Radius = 3 });
+			circles.Add(new Circle { X = 2, Y = 15, Radius = 1 });
+
+			List<Square> squares = new List<Square>();
+			squares.Add(new Square { X = 3, Y = 7, Length = 15 });
+			squares.Add(new Square { X = 3, Y = 10, Length = 23 });
+			squares.Add(new Square { X = 7, Y = 0, Length = 2 });
+
+			foreach (var c in circles)
+				c.Draw();
+
+			foreach (var s in squares)
+			    s.Draw();
+
+			Console.ReadLine();
+		}
+	}
+
+	public class Circle
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Radius { get; set; }
+
+		public void Draw()
+		{
+			Console.WriteLine(String.Format("Circle: {0} {1} {2}", X, Y, Radius));
+		}
+	}
+
+	public class Square
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Length { get; set; }
+
+		public void Draw()
+		{
+			Console.WriteLine(String.Format("Square: {0} {1} {2}", X, Y, Length));
+		}
+	}
+}
---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting
+{
+	// The output must display the items sorted in ascending order.
+	// Notes:
+	//     Main method cannot be modified.
+	//     The base class for Entity cannot be changed.
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Entity e = new Entity();
+			e.Add(5);
+			e.Add(2);
+			e.Add(10);
+			e.Add(9);
+			e.Add(1);
+			e.Add(3);
+			e.Add(4);
+
+			foreach (int i in e)
+				Console.WriteLine(i);
+
+			Console.ReadLine();
+		}
+	}
+
+	class Entity : System.Collections.ArrayList
+	{
+	}
+}
