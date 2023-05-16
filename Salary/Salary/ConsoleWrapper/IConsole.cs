f582d8ce4a3cf6a35446edb5524c31b70d9b6327
Joseph Suchar <joseph.suchar@modmed.com>
2023-05-16 19:01:55 -0400

    Added TDD complexity to the Salary exercise

---
+++ Salary/Salary/ConsoleWrapper/IConsole.cs
@@ -0,0 +1,20 @@
+﻿namespace Salary.ConsoleWrapper
+{
+    /// <summary>
+    /// <see cref="System.Console"/> wrapper contract.
+    /// </summary>
+    internal interface IConsole
+    {
+        /// <summary>Writes the specified string value, followed by the current line terminator, to the standard output stream.</summary>
+        /// <param name="value">The value to write.</param>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurred.</exception>
+        void WriteLine(string value);
+
+        /// <summary>Reads the next line of characters from the standard input stream.</summary>
+        /// <returns>The next line of characters from the input stream, or <see langword="null" /> if no more lines are available.</returns>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurred.</exception>
+        /// <exception cref="T:System.OutOfMemoryException">There is insufficient memory to allocate a buffer for the returned string.</exception>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">The number of characters in the next line of characters is greater than <see cref="F:System.Int32.MaxValue" />.</exception>
+        string ReadLine();
+    }
+}
